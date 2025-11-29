60b098a08950d87c06f36234438e5f81f4ba0695
Arnav242005 <jadhavarnav58@gmail.com>
2025-11-29 20:41:03 +0530

    added changes on ngos model

--- src/main/java/com/arnav/demo/model/Ngo.java
+++ src/main/java/com/arnav/demo/model/Ngo.java
@@ -3,6 +3,7 @@ package com.arnav.demo.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,8 @@ public class Ngo {
 
     @Column(length = 30)
     private String ngo_name;
+
+    @Column(length = 20,unique = true)
+    @NotNull
+    private String registration_no;
 }
